622320daf8ada05b852b4bf1c67cb9ba17013814
Pulse Habour <189056730+PulseHabour@users.noreply.github.com>
2025-06-09 15:22:32 +0100

    fix: Add rdr3 warning

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -14,3 +14,5 @@ files {
     'html/index.html',
     'html/script.js'
 }
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
